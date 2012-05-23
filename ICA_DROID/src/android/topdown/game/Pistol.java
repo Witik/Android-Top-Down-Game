c3d733fd0f17cc8c0b0b511a459bedb4a0b200e0
Thomas van Putten <thomasvanputten@hotmail.com>
2012-05-23 10:32:32 +0200

    Gun Aanpassing Met static final voor externe vergelijkingen

--- ICA_DROID/src/android/topdown/game/Pistol.java
+++ ICA_DROID/src/android/topdown/game/Pistol.java
@@ -1,18 +1,20 @@
 package android.topdown.game;
 
 public class Pistol extends Gun {
-
+	public static final int MAX_AMMO = 255;
+	public static final int DAMAGE = 10;
+	
 	public Pistol(int x, int y, int ammo) {
-		super(x, y, "pistol", ammo, 255, 10);
+		super(x, y, "pistol", ammo, MAX_AMMO, DAMAGE);
 	}
 	
 	public Pistol(int ammo){
-		super("pistol", ammo, 255, 10);
+		super("pistol", ammo, MAX_AMMO, DAMAGE);
 	}
 
 	@Override
 	public void shoot(double x, double y, int rotation) {
-		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*6)-3)), 10, 30, 30);
+		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*6)-3)), 10, 30, DAMAGE);
 	}
 
 
--- ICA_DROID/src/android/topdown/game/Shotgun.java
+++ ICA_DROID/src/android/topdown/game/Shotgun.java
@@ -1,20 +1,21 @@
 package android.topdown.game;
 
 public class Shotgun extends Gun {
-
+	public static final int MAX_AMMO = 70;
+	public static final int DAMAGE = 40;
 	public Shotgun(int x, int y, int ammo) {
-		super(x, y, "shotgun", ammo, 70, 40);
+		super(x, y, "shotgun", ammo, MAX_AMMO, DAMAGE);
 	}
 	
 	public Shotgun(int ammo){
-		super("shotgun", ammo, 70, 40);
+		super("shotgun", ammo, MAX_AMMO, DAMAGE);
 	}
 
 	@Override
 	public void shoot(double x, double y, int rotation) {
-		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*30)-15)), 10, 30,50);
-		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*30)+15)), 10, 30,50);
-		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*30)-30)), 10, 30,50);
+		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*30)-15)), 10, 30,DAMAGE);
+		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*30)+15)), 10, 30,DAMAGE);
+		new Bullet(x,y,(int) (rotation+Math.round((Math.random()*30)-30)), 10, 30,DAMAGE);
 	}
 
 }
